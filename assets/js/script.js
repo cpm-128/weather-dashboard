cc55bb6e33fc101001915539f1bb9c9d2526f1ae
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-20 18:31:52 -0400

    form submits

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,30 @@ var userCityNameEl = document.querySelector("#cityName");
 var userStateCodeEl = document.querySelector("#stateCode");
 var userCountryCodeEl = document.querySelector("#countryCode");
 
-
-// ** hardcoded placeholders
-var lat = 33.44;
-var lon = -94.04;
-
-var apiWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey;
-// var getWeatherData = function (lat, lon) { etc...
+// var getWeatherData = function (lat, lon) {
+//     console.log(">>> data >>>" , data, ">>> lat >>>" , lat , ">>> lon >>>");
+//     var apiWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey;
+//     console.log(">>> apiWeatherUrl >>>", apiWeatherUrl);
 // };
-// getWeatherData(lat, lon)
 
 
 var getCoordData = function(cityName, stateCode, countryCode) {
     //format the api url
     var apiCoordUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + "," + countryCode + "&appid=" + apiKey;
-    console.log("apiCoordUrl >>>", apiCoordUrl)
+        console.log("apiCoordUrl >>>", apiCoordUrl)
     // make a request to the url
     fetch(apiCoordUrl)
         .then(function(response) {
-            response.json().then(function(data) {
-                console.log(">>> data >>>" , data);
-            });
-        });
-    // get the lat and lon values from here and return for getWeatherData()
-    };
+            if(response.ok) {
+                response.json().then(function(data) {
+                var lat = data.lat;
+                var lon = data.lon;
+                console.log(">>> lat >>>", lat , ">>> lon >>>" , lon);
+                getWeatherData(lat, lon);
+            })
+        }});
+};
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
