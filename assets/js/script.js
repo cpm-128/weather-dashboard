ebf64bd0ec669a9afa99186dbe9210f42a02dc00
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-20 14:56:13 -0400

    search card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 var apiKey = "943c2d97f00c4a3d1e9383c1afac4cc1";
 
+// ** hardcorded placeholders
 var cityName = "Bohemia";
 var stateCode = "Ny";
 var countryCode = "USA";
 
-apiCoordUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + "," + countryCode + "&appid=" + apiKey;
-console.log(apiCoordUrl);
 
+
+// ** hardcoded placeholders
 var lat = 33.44;
 var lon = -94.04;
 
-apiWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey;
-console.log(apiWeatherURL);
+var apiWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey;
+
+
+
+var getCoordData = function() {
+    //format the api url
+    var apiCoordUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + "," + countryCode + "&appid=" + apiKey;
+
+    // make a request to the url
+    fetch(apiCoordUrl)
+        .then(function(response) {
+            response.json().then(function(data) {
+                console.log(">>> data >>>" , data);
+            });
+        });
+    };
+getCoordData();
+
+
+
 
